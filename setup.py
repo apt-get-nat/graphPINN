3431576a98c274cc36425f45d6f68dcd03f7418b
Nat Mathews <nathanielhm@gmail.com>
2022-11-10 16:02:01 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+install_requires = ["torch", "torch-sparse", "torch-scatter", "torch-geometric"]
+
+setup(
+  name="graphPINN",
+  version = 0.0.1,
+  maintainer="Nat Mathews",
+  license="GNU3",
+  packages=find_packages(),
+  install_requires=install_requires,
+  python_requires=">=3.9",
+)
