d1b32cb864bfc793f888b10d74adacfd8330c821
Nat Mathews <nathanielhm@gmail.com>
2022-11-11 10:54:53 -0500

    Update __init__.py

--- graphPINN/__init__.py
+++ graphPINN/__init__.py
@@ -2,5 +2,7 @@ from graphPINN.core import *
 import graphPINN.MHS
 import graphPINN.data
 
+graphPINN.__version__ = "0.0.1" # this is bad practice and should be changed
+
 def __init__():
     return
